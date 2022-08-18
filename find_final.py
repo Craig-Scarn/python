0694a6daa0a3ed597bff4dda40be6c1eb55c6aa9
craig.robert.gray <craig.robert.gray@gmail.com>
2022-08-18 03:45:45 -0500

    test

---
+++ find_final.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import os
+import re
+cmd = 'ls -l'
+pattern = '[Pp][Aa][Ss][Ss][Ww][Oo][Rr][Dd]'
+
+#Get list of files
+output = os.popen(cmd).read()
+line = output.rstrip()
+line = output.split()
+#print(line)
+
+files = []
+for i in line:
+    if 'txt' in i:
+        files.append(i)
+
+#print(files)
+line2 = '' 
+for item in files:
+    hand = open(item)
+    for line in hand:
+        line = line.rstrip()
+        if re.findall(pattern, line):
+            print(item, '---', line)
+
+
+
+
+'''
+for line in hand:
+    line = line.rstrip()
+    if re.findall(pattern, line):
+        print(line)'''
